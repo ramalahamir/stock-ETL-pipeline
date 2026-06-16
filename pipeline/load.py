52282ffe67d3f09ce52504385beaa23c6b8b45bf
ramalah amir <ramalahamir@gmail.com>
2026-06-16 13:37:11 +0500

    updated load.py to load the csvs to the S3 bucket processed folder

--- pipeline/load.py
+++ pipeline/load.py
@@ -1,11 +1,32 @@
 import pandas as pd
+import boto3
 import logging
+from io import StringIO
 from sqlalchemy import create_engine
-from config import DB_URL, PROCESSED_DATA_DIR
+from config import (
+    DB_URL,
+    AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY,
+    AWS_BUCKET_NAME,
+    AWS_REGION,
+    S3_PROCESSED_DATA_FOLDER
+)
 
 logger = logging.getLogger(__name__)
 
 def load_data(tables: dict[str, pd.DataFrame]) -> None:
+    """
+    Loads all transformed tables into SQLite database and uploads to S3.
+    """
+    try:
+        load_to_database(tables)
+        upload_to_s3(tables)
+    except Exception as e:
+        logger.error(f"Error during loading: {e}")
+        raise
+
+
+def load_to_database(tables: dict[str, pd.DataFrame]) -> None:
     """
     Loads all transformed tables into SQLite database.
     """
@@ -22,3 +43,31 @@ def load_data(tables: dict[str, pd.DataFrame]) -> None:
     except Exception as e:
         logger.error(f"Error occurred while loading data into the database: {e}")
         raise
+
+def upload_to_s3(tables: dict[str, pd.DataFrame]) -> None:
+    """
+    Uploads all processed tables as CSVs to S3 processed/ folder.
+    """
+    try:
+        logger.info("Connecting to S3...")
+        s3_client = boto3.client(
+            's3',
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+            region_name=AWS_REGION
+        )
+
+        for table_name, df in tables.items():
+            csv_buffer = StringIO()
+            df.to_csv(csv_buffer, index=False)
+
+            s3_key = f"{S3_PROCESSED_DATA_FOLDER}/{table_name}.csv"
+            logger.info(f"Uploading '{table_name}' to S3 at '{s3_key}'...")
+            s3_client.put_object(Bucket=AWS_BUCKET_NAME, Key=s3_key, Body=csv_buffer.getvalue().encode('utf-8'))
+            logger.info(f"Table '{table_name}' uploaded successfully to S3.")
+        
+        logger.info("All tables uploaded successfully to S3.")
+    except Exception as e:
+        logger.error(f"Error occurred while uploading data to S3: {e}")
+        raise
+    
